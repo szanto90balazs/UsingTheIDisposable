895708e4a545397233d8b4cd2b044ce7375e0e63
Balázs Szántó <balazs.szanto@prezi.com>
2016-07-30 10:24:14 +0200

    add code for part I.
    Using the IDisposable - the story of the temp directory

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Threading.Tasks;
 
 namespace UsingTheIDisposable
 {
@@ -8,7 +7,102 @@ namespace UsingTheIDisposable
 	{
 		public static void Main()
 		{
-			
+			//Part I.
+			//ShowCaseTemporaryDirectory.Run();
+			//ShowCaseTemporaryDirectory.RunWithoutFluff();
 		}
 	}
+
+#region Part I.
+
+	public class TemporaryDirectory : IDisposable
+	{
+		public string Path { get; }
+
+		public TemporaryDirectory(string directoryPath)
+		{
+			Path = directoryPath;
+
+			Directory.CreateDirectory(Path);
+		}
+
+		public void Dispose()
+		{
+			Directory.Delete(Path, true);
+		}
+	}
+
+	public class ApplicationStorage
+	{
+		public TemporaryDirectory CreateTemporaryDirectory()
+		{
+			var tempPath = Path.GetTempPath();
+			var tempDirectoryName = Path.GetRandomFileName();
+			var tempDirectoryFullPath = Path.Combine(
+				tempPath,
+				"MyAwesomeInc",
+				tempDirectoryName);
+
+			return new TemporaryDirectory(tempDirectoryFullPath);
+		}
+	}
+
+	public static class ShowCaseTemporaryDirectory
+	{
+		public static void Run()
+		{
+			var applicationStorage = new ApplicationStorage();
+
+			using (var temporaryDirectory = applicationStorage.CreateTemporaryDirectory())
+			{
+				var filePath = Path.Combine(temporaryDirectory.Path, "content.txt");
+				File.WriteAllText(filePath, "Adding new content");
+
+				/*
+				 * Doing serious business here. Really.
+				 * Temporary computations, file manipulations and what not.
+				 */
+
+				/*
+				 * Persisting the result to a permanent storage.
+				 */
+
+			} //Put a breakpoint here, and check your %temp%\MyAwesomeInc\ folder
+
+			//Check again your %temp%\MyAwesomeInc\
+		}
+
+		public static void RunWithoutFluff()
+		{
+			var tempPath = Path.GetTempPath();
+			var tempDirectoryName = Path.GetRandomFileName();
+			var tempDirectoryFullPath = Path.Combine(
+				tempPath,
+				"AwesomeInc",
+				tempDirectoryName);
+
+			try
+			{
+				Directory.CreateDirectory(tempDirectoryFullPath);
+
+				var filePath = Path.Combine(tempDirectoryFullPath, "content.txt");
+				File.WriteAllText(filePath, "Adding new content");
+
+				/*
+				 * Doing serious business here. Really.
+				 * Temporary computations, file manipulations and what not.
+				 */
+
+				/*
+				 * Persisting the result to a permanent storage.
+				 */
+			}
+			finally
+			{
+				Directory.Delete(tempDirectoryFullPath, true);
+			}
+		}
+	}
+
+#endregion
 }
